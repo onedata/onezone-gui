5655876ac529ba762fc726dc8e72d87eb122912c
Jakub Liput <jakub.liput@gmail.com>
2017-08-18 11:57:30 +0200

    VFS-3450 Copying dependencies from addon to project

--- src/app/router.js
+++ src/app/router.js
@@ -1,4 +1,14 @@
+/**
+ * Using standard routes and configuration from onedata-gui-common addon
+ * @module router
+ * @author Jakub Liput
+ * @copyright (C) 2017 ACK CYFRONET AGH
+ * @license This software is released under the MIT license cited in 'LICENSE.txt'.
+ */
+
 import Ember from 'ember';
+import onedataRouterSetup from 'onedata-gui-common/utils/onedata-router-setup';
+
 import config from './config/environment';
 
 const Router = Ember.Router.extend({
@@ -6,7 +16,8 @@ const Router = Ember.Router.extend({
   rootURL: config.rootURL,
 });
 
-Router.map(function() {
+Router.map(function () {
+  onedataRouterSetup(Router, this);
 });
 
 export default Router;
