fda52396b325c4f7462c381c181d795ceb188081
Michal Borzecki <michal.borzecki@hotmail.com>
2018-01-15 08:28:17 +0100

    VFS-3955 fixed client token search in sidebar

--- src/lib/onedata-gui-websocket-client/addon/models/client-token.js
+++ src/lib/onedata-gui-websocket-client/addon/models/client-token.js
@@ -7,7 +7,9 @@
 
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
+import { computed } from '@ember/object';
 
 export default Model.extend({
   token: attr('string'),
+  name: computed.reads('token'),
 });
