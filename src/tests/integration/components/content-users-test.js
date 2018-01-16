6786d712c13ad7b1835d3b21f90c22ff051de13e
Michal Borzecki <michal.borzecki@hotmail.com>
2018-01-16 12:12:17 +0100

    VFS-3571 one-inline-editor

--- src/tests/integration/components/content-users-test.js
+++ src/tests/integration/components/content-users-test.js
@@ -3,13 +3,13 @@ import { describe, it } from 'mocha';
 import { setupComponentTest } from 'ember-mocha';
 import hbs from 'htmlbars-inline-precompile';
 
-describe('Integration | Component | content users', function() {
+describe('Integration | Component | content users', function () {
   setupComponentTest('content-users', {
-    integration: true
+    integration: true,
   });
 
-  it('renders', function() {
-    this.render(hbs`{{content-users}}`);
+  it('renders', function () {
+    this.render(hbs `{{content-users}}`);
     expect(this.$()).to.have.length(1);
   });
 });
