4d9e6310d138f40c1173314f67bae43407df6588
Michal Borzecki <michal.borzecki@hotmail.com>
2018-01-24 11:26:22 +0100

    Squashed 'src/lib/onedata-gui-common/' changes from 40fb7f3..49f154b
    49f154b VFS-3968 overflow detector 'set on destroyed' error fixed
    4546c19 VFS-3968 new ember init
    ab47f60 Merge commit '9a2e3d5e5c3d65afbadcf9e788e132c1fc1cc39c' into feature/VFS-3968-upgrade-ember-to-2.18
    895553f VFS-3968 fixed eslint issues
    8cc663a VFS-3968 ember upgrade
    160b146 Merge pull request #34 in VFS/onedata-gui-common from feature/VFS-3571-list-of-auth-providers to develop
    aeffbd6 Merge commit '369e148ca0361220d5cfc394e35628e7731fe218' into feature/VFS-3571-list-of-auth-providers
    74e4848 VFS-3571 PR fixes
    1e4db56 Merge commit 'e4dff5965101749f8a5780bb6eff0f5675717027' into feature/VFS-3571-list-of-auth-providers
    3d0c199 Merge branch 'develop' into feature/VFS-3571-list-of-auth-providers
    fca0d99 VFS-3571 inline-editor display improvements
    77dbf7c VFS-3571 improvements in profile page layout
    24e8ac4 VFS-3571 one-inline-editor
    de3d814 Merge commit 'ebec82504c0a46993310df189af1c7015b36c4b5' into feature/VFS-3955-tokens-view
    a939233 VFS-3571 basic user account page
    6290af1 Merge commit '6bee720399ea9e123b536e43a8073e7763d9fd12' into feature/VFS-3955-tokens-view
    6039936 Merge commit '71ebb5817e216377e5adcedee0425f8a267b28b4' into feature/VFS-3619-port-oz-gui-default-login
    96b19eb Merge commit 'a86a5701a1fc39f3217d5966464e5de54b53d151' into feature/VFS-3619-port-oz-gui-default-login
    cfee2af VFS-3205 Deployment step translations and handling empty steps
    c4ab8c8 VFS-3706 Not allowing to enter provider routes in zone panel
    
    git-subtree-dir: src/lib/onedata-gui-common
    git-subtree-split: 49f154bef69a201026e25d2a1f3a8d58fe6221d5

--- addon/components/user-account-button-ws.js
+++ addon/components/user-account-button-ws.js
@@ -5,46 +5,37 @@
  * NOTE: requires onedata websocket managed session
  *
  * @module components/user-account-button-ws
- * @author Jakub Liput
- * @copyright (C) 2017 ACK CYFRONET AGH
+ * @author Jakub Liput, Michal Borzecki
+ * @copyright (C) 2017-2018 ACK CYFRONET AGH
  * @license This software is released under the MIT license cited in 'LICENSE.txt'.
  */
 
-import Ember from 'ember';
+import { reads } from '@ember/object/computed';
+
+import { inject as service } from '@ember/service';
 
 import UserAccountButton from 'onedata-gui-common/components/user-account-button';
 import layout from 'onedata-gui-common/templates/components/user-account-button';
-
-const {
-  inject: { service },
-  get,
-} = Ember;
 
 export default UserAccountButton.extend({
   layout,
 
   currentUser: service(),
-  globalNotify: service(),
+
+  /**
+   * @type {models/user}
+   */
+  user: undefined,
 
   /**
    * @override
    */
-  username: null,
+  username: reads('user.name'),
 
   init() {
     this._super(...arguments);
-    this.get('currentUser').getCurrentUserRecord().then(user => {
-      this.set('username', get(user, 'name'));
-    })
-  },
-
-  actions: {
-    /**
-     * @override
-     */
-    manageAccount() {
-      // TODO implement
-      this.get('globalNotify').error('Account settings not implemented yet');
-    },
+    this.get('currentUser').getCurrentUserRecord().then(user =>
+      this.set('user', user)
+    );
   },
 });
