f747fb6de391149999cecfa17bbf3417299e6bec
Jakub Liput <jakub.liput@gmail.com>
2018-02-07 12:16:07 +0100

    VFS-3952 Fixing generate-devlopment-model tests

--- src/app/utils/generate-development-model.js
+++ src/app/utils/generate-development-model.js
@@ -19,7 +19,7 @@ const USERNAME = 'Stub User';
 const USER_LOGIN = 'stub_user';
 const NUMBER_OF_PROVIDERS = 3;
 const NUMBER_OF_SPACES = 3;
-const NUMBER_OF_CLIENT_TOKENS = 0;
+const NUMBER_OF_CLIENT_TOKENS = 3;
 const LINKED_ACCOUNT_TYPES = ['plgrid', 'indigo', 'google'];
 
 const providerStatusList = ['online', 'offline'];
@@ -69,8 +69,8 @@ export default function generateDevelopmentModel(store) {
 
 function createUserRecord(store, listRecords) {
   const spacesIndex = types.indexOf('space');
-  listRecords[spacesIndex].get('list')
-    .then(list => list.get('firstObject'))
+  return listRecords[spacesIndex].get('list')
+    .then(list => list.get('length') > 0 ? list.get('firstObject ') : null)
     .then(space => space && space.get('entityId'))
     .then(defaultSpaceId => {
       const userRecord = store.createRecord('user', {
@@ -117,7 +117,8 @@ function createProvidersRecords(store) {
     let sign = index % 2 ? -1 : 1;
     return store.createRecord('provider', {
       name: `Provider ${index}`,
-      latitude: ((180 / (NUMBER_OF_PROVIDERS + 1)) * (index + 1) - 90) * sign,
+      latitude: ((180 / (NUMBER_OF_PROVIDERS + 1)) * (index + 1) - 90) *
+        sign,
       longitude: (360 / (NUMBER_OF_PROVIDERS + 1)) * (index + 1) - 180,
       status: providerStatusList[index % providerStatusList.length],
       host: `10.0.0.${index + 1}`,
