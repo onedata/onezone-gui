a6b493b7d8dd3abc8fb5cc06d7a450e4fa0b221e
Jakub Liput <jakub.liput@gmail.com>
2018-02-20 15:36:07 +0100

    VFS-3952 Sorting spaces in sidebar

--- src/app/components/sidebar-tokens.js
+++ src/app/components/sidebar-tokens.js
@@ -7,6 +7,8 @@
  * @license This software is released under the MIT license cited in 'LICENSE.txt'.
  */
 
+import { computed } from '@ember/object';
+import { A } from '@ember/array';
 import TwoLevelSidebar from 'onedata-gui-common/components/two-level-sidebar';
 import layout from 'onedata-gui-common/templates/components/two-level-sidebar';
 
@@ -19,6 +21,14 @@ export default TwoLevelSidebar.extend({
    * @override
    */
   model: null,
+
+  /**
+   * @override
+   * Keep original sorting from server, but make a copy of array
+   */
+  sortedCollection: computed('model.collection.[]', function () {
+    return A(this.get('model.collection').toArray());
+  }),
 
   /**
    * @override
