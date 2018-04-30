83cf2cb45ed24cfd48a250673bdb7a6be357b59f
Michal Borzecki <michal.borzecki@hotmail.com>
2018-04-30 17:02:33 +0200

    VFS-4400 draft of the new providers map

--- src/app/components/content-providers.js
+++ src/app/components/content-providers.js
@@ -8,13 +8,14 @@
  */
 
 import Component from '@ember/component';
-import { computed } from '@ember/object';
-import { readOnly } from '@ember/object/computed';
+import { computed, get } from '@ember/object';
+import { reads } from '@ember/object/computed';
 import { inject } from '@ember/service';
 import PromiseObject from 'onedata-gui-common/utils/ember/promise-object';
 import clusterizeProviders from 'onedata-gui-common/utils/clusterize-providers-by-coordinates';
 import $ from 'jquery';
 import safeExec from 'onedata-gui-common/utils/safe-method-execution';
+import _ from 'lodash';
 
 export default Component.extend({
   classNames: ['content-providers'],
@@ -22,6 +23,7 @@ export default Component.extend({
   providerManager: inject(),
   router: inject(),
   guiUtils: inject(),
+  navigationState: inject(),
 
   /**
    * Selected (active) provider
@@ -36,6 +38,78 @@ export default Component.extend({
   providerList: null,
 
   /**
+   * @type {Object}
+   */
+  _mapState: undefined,
+
+  /**
+   * @type {number}
+   */
+  _mapCalculatedAreaPadding: 2,
+
+  /**
+   * @type {Ember.ComputedProperty<object>}
+   */
+  _mapDefaultState: computed(
+    '_providers.@each.{latitude,longitude}',
+    '_mapCalculatedAreaPadding',
+    function () {
+      const {
+        _providers,
+        _mapCalculatedAreaPadding,
+      } = this.getProperties('_providers', '_mapCalculatedAreaPadding');
+      if (!_providers || get(_providers, 'length') === 0) {
+        return {
+          lat: 0,
+          lng: 0,
+          scale: 0,
+        };
+      } else {
+        const latitudes = _providers.map(p => get(p, 'latitude'));
+        const longitudes = _providers.map(p => get(p, 'longitude'));
+        const minLat = Math.min(...latitudes);
+        const maxLat = Math.max(...latitudes);
+        const minLng = Math.min(...longitudes);
+        const maxLng = Math.max(...longitudes);
+        const areaWidth = (maxLng - minLng) * _mapCalculatedAreaPadding;
+        const areaHeight = (maxLat - minLat) * _mapCalculatedAreaPadding;
+        const xScale = 360 / areaWidth;
+        const yScale = 180 / areaHeight;
+        const scale = Math.max(1, Math.min(xScale, yScale));
+        return {
+          lat: (minLat + maxLat) / 2,
+          lng: (minLng + maxLng) / 2,
+          scale,
+        };
+      }
+    }
+  ),
+
+  /**
+   * Map state passed via query params
+   * @type {Ember.ComputedProperty<object>}
+   */
+  _mapInitialState: computed(
+    'navigationState.queryParams',
+    '_mapDefaultState',
+    function () {
+      const queryParams = this.get('navigationState.queryParams');
+      const mapStateFromQuery = Object.keys(queryParams).reduce((s, key) => {
+        if (_.startsWith(key, 'map')) {
+          const newKey = _.lowerFirst(key.substring(3));
+          s[newKey] = queryParams[key];
+        }
+        return s;
+      }, {});
+      if (Object.keys(mapStateFromQuery).length > 0) {
+        return mapStateFromQuery;
+      } else {
+        return this.get('_mapDefaultState');
+      }
+    }
+  ),
+
+  /**
    * @type {Ember.ComputedProperty<PromiseObject<DS.RecordArray<Provider>>>>}
    */
   _providersProxy: computed('providerList.list', function () {
@@ -48,15 +122,24 @@ export default Component.extend({
    * Array of all prviders
    * @type {Ember.ComputedProperty<DS.RecordArray<Provider>>>}
    */
-  _providers: readOnly('_providersProxy.content'),
+  _providers: reads('_providersProxy.content'),
 
   /**
    * Clustered providers
    * @type {Ember.ComputedProperty<Array<Object>>}
    */
-  _clusteredProviders: computed('_providers', function _clusteredProviders() {
-    return clusterizeProviders(this.get('_providers') || []);
-  }),
+  _clusteredProviders: computed(
+    '_providers',
+    '_mapState',
+    function _clusteredProviders() {
+      const {
+        _providers,
+        _mapState,
+      } = this.getProperties('_providers', '_mapState');
+      const squareSideLength = 10 / (_mapState.scale || 1);
+      return clusterizeProviders(_providers || [], squareSideLength, squareSideLength);
+    }
+  ),
 
   /**
    * If true, page component has the mobile layout
@@ -78,12 +161,22 @@ export default Component.extend({
    */
   _window: window,
 
+  init() {
+    this._super(...arguments);
+    this.set('_mapState', {
+      lat: 0,
+      lng: 0,
+      scale: 1,
+    });
+  },
+
   didInsertElement() {
     this._super(...arguments);
     let {
       _window,
       _windowResizeHandler,
-    } = this.getProperties('_window', '_windowResizeHandler');
+      _mapState,
+    } = this.getProperties('_window', '_windowResizeHandler', '_mapState');
     $(_window).on('resize', _windowResizeHandler);
     this._windowResized();
     this.$().click((event) => {
@@ -92,10 +185,13 @@ export default Component.extend({
         if (!this.get('_mobileMode')) {
           if (!target.hasClass('provider-place') &&
             target.parents('.provider-place').length === 0) {
+            const queryMapState =
+              _.mapKeys(_mapState || {}, (v, k) => 'map' + _.upperFirst(k));
             this.get('router').transitionTo(
-              'onedata.sidebar.content',
+              'onedata.sidebar.content.aspect',
               'providers',
-              'not-selected'
+              'not-selected',
+              'index', { queryParams: queryMapState }
             );
           }
         }
@@ -124,14 +220,12 @@ export default Component.extend({
   },
 
   actions: {
-    providerChanged(provider) {
-      const guiUtils = this.get('guiUtils');
-      this.get('router').transitionTo(
-        'onedata.sidebar.content.aspect',
-        'providers',
-        guiUtils.getRoutableIdFor(provider),
-        'index'
-      );
+    mapViewportChanged(event) {
+      this.set('_mapState', {
+        lat: event.lat,
+        lng: event.lng,
+        scale: event.scale,
+      });
     },
   },
 });
