da2660d336301a6a89d51e3259f0988b0ab003f8
Michal Borzecki <michal.borzecki@hotmail.com>
2018-05-25 10:30:07 +0200

    VFS-4401 tests

--- src/lib/onedata-gui-websocket-client/tests/unit/utils/privileges-array-to-object-test.js
+++ src/lib/onedata-gui-websocket-client/tests/unit/utils/privileges-array-to-object-test.js
@@ -1,11 +1,28 @@
 import { expect } from 'chai';
 import { describe, it } from 'mocha';
 import privilegesArrayToObject from 'onedata-gui-websocket-client/utils/privileges-array-to-object';
+import flatFlags, { groupedFlags } from 'onedata-gui-websocket-client/utils/group-privileges-flags';
+
+function getTreeSumedValue(tree, useAnd) {
+  return Object.keys(tree).reduce((val, groupName) => {
+    return Object.keys(tree[groupName]).reduce((inVal, privName) => {
+      if (useAnd) {
+        return inVal && tree[groupName][privName];
+      } else {
+        return inVal || tree[groupName][privName];
+      }
+    }, val);
+  }, useAnd);
+}
 
 describe('Unit | Utility | privileges array to object', function() {
-  // Replace this with your real tests.
-  it('works', function() {
-    let result = privilegesArrayToObject();
-    expect(result).to.be.ok;
+  it('marks privileges as true if available', function() {
+    let result = privilegesArrayToObject(flatFlags, groupedFlags);
+    expect(getTreeSumedValue(result, true)).to.be.true;
+  });
+
+  it('marks privileges as false if not available', function() {
+    let result = privilegesArrayToObject([], groupedFlags);
+    expect(getTreeSumedValue(result, false)).to.be.false;
   });
 });
