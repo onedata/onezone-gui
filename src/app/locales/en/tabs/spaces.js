348ecb178e9b282401256b2aac08e902f3059710
Michal Borzecki <michal.borzecki@hotmail.com>
2018-05-30 13:03:01 +0200

    VFS-4429 empty privileges aspect

--- src/app/locales/en/tabs/spaces.js
+++ src/app/locales/en/tabs/spaces.js
@@ -2,6 +2,7 @@ export default {
   aspects: {
     index: 'Space overview',
     providers: 'Space providers',
+    privileges: 'Space privileges',
     support: 'Get support',
     notFound: 'Space not found',
   },
