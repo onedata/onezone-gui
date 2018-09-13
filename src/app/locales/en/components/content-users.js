12ae8ea19b681a018b49ca7bbafac278311decb3
Michal Borzecki <michal.borzecki@hotmail.com>
2018-09-13 16:55:07 +0200

    VFS-4776 reactive user content page

--- src/app/locales/en/components/content-users.js
+++ src/app/locales/en/components/content-users.js
@@ -1,7 +1,7 @@
 export default {
   profile: 'Profile',
   name: 'Name',
-  login: 'Login',
+  alias: 'Alias',
   linkedAccounts: 'Linked accounts',
   userDataPersistence: 'user data persistence',
   addAccount: 'Add account...',
