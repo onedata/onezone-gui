40b82fd71602b868b8d68e975a62696d999ef004
Michal Borzecki <michal.borzecki@hotmail.com>
2018-09-19 11:13:44 +0200

    VFS-4826 added extra space privileges

--- src/app/locales/en/components/content-spaces-members.js
+++ src/app/locales/en/components/content-spaces-members.js
@@ -5,27 +5,37 @@ export default {
   privilegeGroups: {
     spaceManagement: 'Space management',
     dataManagement: 'Data management',
+    transferManagement: 'Transfer management',
     userManagement: 'User management',
     groupManagement: 'Group management',
     providerManagement: 'Provider management',
   },
   privileges: {
     space_view: 'View space',
-    space_write_data: 'Write files',
-    space_manage_shares: 'Manage shares',
     space_update: 'Modify space',
+    space_delete: 'Remove space',
     space_view_privileges: 'View privileges',
     space_set_privileges: 'Set privileges',
-    space_delete: 'Remove space',
+
+    space_read_data: 'Read files',
+    space_write_data: 'Write files',
+    space_manage_shares: 'Manage shares',
+    space_manage_indexes: 'Manage indexes',
+    space_query_indexes: 'Query indexes',
+    space_view_statistics: 'View statistics',
+
     space_view_transfers: 'View transfers',
     space_schedule_replication: 'Schedule replication',
     space_cancel_replication: 'Cancel replication',
     space_schedule_eviction: 'Schedule eviction',
     space_cancel_eviction: 'Cancel eviction',
+
     space_invite_user: 'Invite user',
     space_remove_user: 'Remove user',
+
     space_add_group: 'Add group',
     space_remove_group: 'Remove group',
+
     space_invite_provider: 'Invite provider',
     space_remove_provider: 'Remove provider',
   },
