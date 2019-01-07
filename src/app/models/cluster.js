2359e381ea03feedf9e650cf2b5f4fa04b304168
Jakub Liput <jakub.liput@gmail.com>
2019-01-07 17:59:24 +0100

    WIP

--- src/app/models/cluster.js
+++ src/app/models/cluster.js
@@ -7,12 +7,37 @@
 
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
+import { belongsTo } from 'onedata-gui-websocket-client/utils/relationships';
 import GraphSingleModelMixin from 'onedata-gui-websocket-client/mixins/models/graph-single-model';
+import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default Model.extend(GraphSingleModelMixin, {
+  onedataConnection: service(),
+
   type: attr('string'),
-  serviceId: attr('string'),
-  name: attr('string'),
-  domain: attr('string'),
+  provider: belongsTo('provider'),
   onepanelProxy: attr('boolean'),
+
+  name: computed('type', 'provider.content.name', function name() {
+    if (this.get('type') === 'oneprovider') {
+      return this.get('provider.content.name');
+    } else {
+      return this.get('onedataConnection.zoneName');
+    }
+  }),
+
+  domain: computed('type', 'provider.content.domain', function domain() {
+    if (this.get('type') === 'oneprovider') {
+      return this.get('provider.content.domain');
+    } else {
+      return location.hostname;
+    }
+  }),
+
+  init() {
+    this._super(...arguments);
+    // force get provider to have fields like name and domain
+    this.get('provider');
+  },
 });
