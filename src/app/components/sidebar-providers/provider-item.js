35b77b13acebf8d960746d9e8f914cfdd736d0ea
Jakub Liput <jakub.liput@gmail.com>
2019-05-09 10:28:43 +0200

    VFS-5342 Refactor of providers sidebar

--- src/app/components/sidebar-providers/provider-item.js
+++ src/app/components/sidebar-providers/provider-item.js
@@ -8,9 +8,9 @@
  */
 
 import { reads } from '@ember/object/computed';
-import { computed } from '@ember/object';
+import { not } from '@ember/object/computed';
 import Component from '@ember/component';
-import { get } from '@ember/object';
+import { computed, get } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 import UserProxyMixin from 'onedata-gui-websocket-client/mixins/user-proxy';
@@ -42,7 +42,7 @@ export default Component.extend(I18n, UserProxyMixin, {
    */
   providerId: reads('provider.entityId'),
 
-  online: reads('provider.online'),
+  offline: not('provider.online'),
 
   /**
    * Icon class based on item status
