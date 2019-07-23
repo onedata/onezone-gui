0a8f413f8a593fb2e36fbe054cc1f74d59205045
Michał Borzęcki <michal.borzecki@hotmail.com>
2019-07-23 13:03:43 +0200

    VFS-5641 added model revision check

--- src/app/models/linked-account-list.js
+++ src/app/models/linked-account-list.js
@@ -1,13 +1,14 @@
 /**
  * @module models/linked-account-list
- * @author Michal Borzecki
- * @copyright (C) 2018 ACK CYFRONET AGH
+ * @author Michał Borzęcki
+ * @copyright (C) 2019-2019 ACK CYFRONET AGH
  * @license This software is released under the MIT license cited in 'LICENSE.txt'.
  */
 
 import Model from 'ember-data/model';
 import { hasMany } from 'onedata-gui-websocket-client/utils/relationships';
+import GraphListModelMixin from 'onedata-gui-websocket-client/mixins/models/graph-list-model';
 
-export default Model.extend({
+export default Model.extend(GraphListModelMixin, {
   list: hasMany('linkedAccount'),
 });
--- src/app/models/provider-list.js
+++ src/app/models/provider-list.js
@@ -1,14 +1,15 @@
 /**
  * @module models/provider-list
- * @author Jakub Liput
- * @copyright (C) 2017-2018 ACK CYFRONET AGH
+ * @author Jakub Liput, Michał Borzęcki
+ * @copyright (C) 2017-2019 ACK CYFRONET AGH
  * @license This software is released under the MIT license cited in 'LICENSE.txt'.
  */
 
 import Model from 'ember-data/model';
 import { hasMany } from 'onedata-gui-websocket-client/utils/relationships';
 import listConflictModel from 'onedata-gui-websocket-client/mixins/models/list-conflict-model';
+import GraphListModelMixin from 'onedata-gui-websocket-client/mixins/models/graph-list-model';
 
-export default Model.extend(listConflictModel, {
+export default Model.extend(GraphListModelMixin, listConflictModel, {
   list: hasMany('provider'),
 });
