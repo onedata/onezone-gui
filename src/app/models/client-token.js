73ad678321f1d93e3da6c4568d4c751233d4bb03
Michał Borzęcki <michal.borzecki@hotmail.com>
2019-10-22 10:19:44 +0200

    VFS-5782 modified client-token model

--- src/app/models/client-token.js
+++ src/app/models/client-token.js
@@ -1,22 +1,236 @@
 /**
  * @module models/client-token
- * @author Michal Borzecki
- * @copyright (C) 2018 ACK CYFRONET AGH
+ * @author Michał Borzęcki
+ * @copyright (C) 2018-2019 ACK CYFRONET AGH
  * @license This software is released under the MIT license cited in 'LICENSE.txt'.
  */
 
-import { computed } from '@ember/object';
+ /**
+ * @typedef {Object} TokenCaveat
+ * @property {string} type
+ * @property {any} * additional options of caveat
+ */
 
+import { computed, observer, getProperties } from '@ember/object';
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
 import StaticGraphModelMixin from 'onedata-gui-websocket-client/mixins/models/static-graph-model';
 import GraphSingleModelMixin from 'onedata-gui-websocket-client/mixins/models/graph-single-model';
+import { reads } from '@ember/object/computed';
+import { reject } from 'rsvp';
+import gri from 'onedata-gui-websocket-client/utils/gri';
+import createDataProxyMixin from 'onedata-gui-common/utils/create-data-proxy-mixin';
+import { cancel, later } from '@ember/runloop';
+import { and, not } from 'ember-awesome-macros';
 
-export default Model.extend(GraphSingleModelMixin, {
-  token: attr('string'),
-  name: computed('token', function () {
-    const token = this.get('token');
-    return token &&
-      token.slice(0, 3) + '...' + token.slice(token.length - 14, token.length);
-  }),
-}).reopenClass(StaticGraphModelMixin);
+const standardGroupMapping = {
+  idFieldName: 'groupId',
+  modelName: 'group',
+};
+
+const standardSpaceMapping = {
+  idFieldName: 'groupId',
+  modelName: 'group',
+};
+
+const standardHarvesterMapping = {
+  idFieldName: 'groupId',
+  modelName: 'group',
+};
+
+const standardClusterMapping = {
+  idFieldName: 'groupId',
+  modelName: 'group',
+};
+
+export const inviteTokenSubtypeToTargetModelMapping = {
+  userJoinGroup: standardGroupMapping,
+  groupJoinGroup: standardGroupMapping,
+  userJoinSpace: standardSpaceMapping,
+  groupJoinSpace: standardSpaceMapping,
+  supportSpace: standardSpaceMapping,
+  registerOneprovider: {
+    idFieldName: 'adminUserId',
+    modelName: 'user',
+  },
+  userJoinCluster: standardClusterMapping,
+  groupJoinCluster: standardClusterMapping,
+  userJoinHarvester: standardHarvesterMapping,
+  groupJoinHarvester: standardHarvesterMapping,
+  spaceJoinHarvester: standardHarvesterMapping,
+};
+
+export default Model.extend(
+  GraphSingleModelMixin,
+  createDataProxyMixin('tokenTarget'), {
+    name: attr('string'),
+    type: attr('object'),
+    rekoved: attr('boolean'),
+    metadata: attr('object'),
+    token: attr('string'),
+
+    /**
+     * Array of caveats
+     * @type {Array<TokenCaveat>}
+     */
+    caveats: attr('array'),
+
+    /**
+     * @type {boolean}
+     */
+    isExpired: false,
+
+    /**
+     * @type {any}
+     */
+    expirationTimer: undefined,
+
+    /**
+     * One of: 'access', 'invite'
+     * @type {Ember.ComputedProperty<string>}
+     */
+    typeName: computed('type', function typeName() {
+      const type = this.get('type') || {};
+
+      if (type.accessToken) {
+        return 'access';
+      } else if (type.inviteToken) {
+        return 'invite';
+      } else {
+        return undefined;
+      }
+    }),
+
+    /**
+     * @type {Ember.ComputedProperty<string|undefined>}
+     * One of:
+     * - undefined (for access token)
+     * - 'userJoinGroup',
+     * - 'groupJoinGroup',
+     * - 'userJoinSpace',
+     * - 'groupJoinSpace',
+     * - 'supportSpace',
+     * - 'registerOneprovider',
+     * - 'userJoinCluster',
+     * - 'groupJoinCluster',
+     * - 'userJoinHarvester',
+     * - 'groupJoinHarvester',
+     * - 'spaceJoinHarvester'
+     */
+    tokenSubtype: reads('type.inviteToken.subtype'),
+
+    /**
+     * UNIX timestamp of token expiration time
+     * @type {Ember.ComputedProperty<number|undefined>}
+     */
+    validUntil: computed('caveats', function validUntil() {
+      const caveats = this.get('caveats') || [];
+      const timeCaveat = caveats.findBy('type', 'time');
+      return timeCaveat ? timeCaveat.validUntil : undefined;
+    }),
+
+    /**
+     * @type {Ember.ComputedProperty<boolean>}
+     */
+    usageLimitReached: computed('metadata', function usageLimitReached() {
+      const metadata = this.get('metadata') || {};
+      const {
+        usageCount,
+        usageLimit,
+      } = getProperties(metadata, 'usageCount', 'usageLimit');
+
+      if (typeof usageCount === 'number' && typeof usageLimit === 'number') {
+        return usageCount >= usageLimit;
+      } else {
+        // Usage limit is not defined
+        return false;
+      }
+    }),
+
+    /**
+     * @type {Ember.ComputedProperty<boolean>}
+     */
+    isActive: and(
+      not('isExpired'),
+      not('usageLimitReached'),
+      not('revoked'),
+    ),
+
+    // FIXME: check if it will be fired up on record load
+    validUntilObserver: observer('validUntil', function () {
+      const {
+        validUntil,
+        isExpired,
+      } = this.getProperties('validUntil', 'isExpired');
+      const nowTimestamp = Date.now();
+      const hasValidUntil = typeof validUntil === 'number';
+
+      if (!hasValidUntil || validUntil >= nowTimestamp) {
+        if (isExpired) {
+          this.set('isExpired', false);
+        }
+      } else {
+        if (!isExpired) {
+          this.set('isExpired', true);
+        }
+      }
+      
+      this.rescheduleExpirationTimer();
+    }),
+
+    init() {
+      this._super(...arguments);
+
+      this.validUntilObserver();
+    },
+
+    destroy() {
+      this._super(...arguments);
+
+      cancel(this.get('expirationTimer'));
+    },
+
+    /**
+     * @override
+     * @return {Promise<Models.User|Models.Group|Models.Cluster|Models.Space>}
+     */
+    fetchTokenTarget() {
+      const {
+        store,
+        type,
+      } = this.getProperties('store', 'type');
+
+      if (!type || !type.inviteToken) {
+        return reject();
+      } else {
+        const targetModelMapping =
+          inviteTokenSubtypeToTargetModelMapping[type.inviteToken.subtype];
+        const targetModelGri = gri({
+          entityType: targetModelMapping.modelName,
+          entityId: type.inviteToken[targetModelMapping.idFieldName],
+          aspect: 'instance',
+          scope: 'auto',
+        });
+
+        return store.findRecord(targetModelMapping.modelName, targetModelGri);
+      }
+    },
+
+    rescheduleExpirationTimer() {
+      const {
+        expirationTimer,
+        validUntil,
+      } = this.getProperties('expirationTimer', 'validUntil');
+      const nowTimestamp = Date.now();
+
+      cancel(expirationTimer);
+      if (typeof validUntil === 'number' && validUntil > nowTimestamp) {
+        const timerTime = (validUntil - nowTimestamp + 1) * 1000;
+        this.set(
+          'expirationTimer',
+          later(() => this.set('isExpired', true), timerTime)
+        );
+      }
+    },
+  }
+).reopenClass(StaticGraphModelMixin);
---
+++ src/tests/unit/models/client-token-test.js
@@ -0,0 +1,261 @@
+import { expect } from 'chai';
+import { describe, it, beforeEach, afterEach } from 'mocha';
+import { setupModelTest } from 'ember-mocha';
+import { set, get, setProperties } from '@ember/object';
+import { inviteTokenSubtypeToTargetModelMapping } from 'onezone-gui/models/client-token';
+import sinon from 'sinon';
+import { resolve } from 'rsvp';
+import gri from 'onedata-gui-websocket-client/utils/gri';
+
+describe('Unit | Model | client token', function() {
+  setupModelTest('client-token', {
+      needs: [],
+  });
+
+  beforeEach(function () {
+    this.clock = sinon.useFakeTimers(Date.now());
+  });
+
+  afterEach(function () {
+    this.clock.restore();
+  });
+
+  [
+    'access',
+    'invite',
+  ].forEach(typeName => {
+    it(`computes "${typeName}" typeName based on type`, function () {
+      const model = this.subject();
+      set(model, 'type', {
+        [typeName + 'Token']: {},
+      });
+
+      expect(get(model, 'typeName')).to.equal(typeName);
+    });
+  });
+
+  it('computes undefined tokenSubtype based on access token type', function () {
+    const model = this.subject();
+    set(model, 'type', {
+      accessToken: {},
+    });
+
+    expect(get(model, 'tokenSubtype')).to.be.undefined;
+  });
+
+  it('computes non-empty tokenSubtype based on invite token type', function () {
+    const subtype = 'userJoinGroup';
+    const model = this.subject();
+    set(model, 'type', {
+      inviteToken: {
+        subtype,
+      },
+    });
+
+    expect(get(model, 'tokenSubtype')).to.equal(subtype);
+  });
+
+  Object.keys(inviteTokenSubtypeToTargetModelMapping).forEach(subtype => {
+    const targetModelMapping = inviteTokenSubtypeToTargetModelMapping[subtype];
+
+    it(
+      `fetches invite token target (${targetModelMapping.modelName}) with subtype ${subtype}`,
+      function () {
+        const targetModelEntityId = 'someId';
+        const targetModelGri = gri({
+          entityType: targetModelMapping.modelName,
+          entityId: targetModelEntityId,
+          aspect: 'instance',
+          scope: 'auto',
+        });
+        const targetModelMock = {
+          targetModelGri,
+          modelName: targetModelMapping.modelName,
+        };
+        sinon.stub(this.store(), 'findRecord')
+          .withArgs(targetModelMapping.modelName, targetModelGri)
+          .returns(resolve(targetModelMock));
+        const model = this.subject();
+        set(model, 'type', {
+          inviteToken: {
+            subtype,
+            [targetModelMapping.idFieldName]: targetModelEntityId,
+          },
+        });
+    
+        return get(model, 'tokenTargetProxy').then(result => {
+          expect(result).to.deep.equal(targetModelMock);
+        });
+      }
+    );
+  });
+
+  it(
+    'presents expiration time in validUntil property if time caveat is defined',
+    function () {
+      const timestamp = 1571677127;
+      const model = this.subject();
+      set(model, 'caveats', [createTimeCaveat(timestamp)]);
+
+      expect(get(model, 'validUntil')).to.equal(timestamp);
+    }
+  );
+
+  it(
+    'presents expiration time in validUntil property as undefined if time caveat is not defined',
+    function () {
+      const model = this.subject();
+      set(model, 'caveats', []);
+
+      expect(get(model, 'validUntil')).to.be.undefined;
+    }
+  );
+
+  it('has usageLimitReached == true if usage limit is reached', function () {
+    const model = this.subject();
+    set(model, 'metadata', {
+      usageCount: 3,
+      usageLimit: 3,
+    });
+
+    expect(get(model, 'usageLimitReached')).to.be.true;
+  });
+
+  it('has usageLimitReached == false if usage limit is not reached', function () {
+    const model = this.subject();
+    set(model, 'metadata', {
+      usageCount: 2,
+      usageLimit: 3,
+    });
+
+    expect(get(model, 'usageLimitReached')).to.be.false;
+  });
+
+  it('has usageLimitReached == false if usage limit is not defined', function () {
+    const model = this.subject();
+
+    expect(get(model, 'usageLimitReached')).to.be.false;
+  });
+
+  it('has isExpired == false when validUntil < now', function () {
+    const model = this.subject();
+    set(model, 'caveats', [createTimeCaveat(Date.now() - 3600)]);
+
+    expect(get(model, 'isExpired')).to.be.true;
+  });
+
+  it('has isExpired == true when validUntil > now', function () {
+    const model = this.subject();
+    set(model, 'caveats', [createTimeCaveat(Date.now() + 3600)]);
+
+    expect(get(model, 'isExpired')).to.be.false;
+  });
+
+  it('schedules change of isExpired if validUntil > now', function () {
+    const model = this.subject();
+    set(model, 'caveats', [createTimeCaveat(Date.now() + 3600)]);
+
+    this.clock.tick(3601 * 1000);
+
+    expect(get(model, 'isExpired')).to.be.true;
+  });
+
+  it('resets scheduled change of isExpired if validUntil has been increased', function () {
+    const model = this.subject();
+    set(model, 'caveats', [createTimeCaveat(Date.now() + 600)]);
+    this.clock.tick(1 * 1000);
+    set(model, 'caveats', [createTimeCaveat(Date.now() + 3600)]);
+    this.clock.tick(600 * 1000);
+
+    expect(get(model, 'isExpired')).to.be.false;
+
+    this.clock.tick(3001 * 1000);
+
+    expect(get(model, 'isExpired')).to.be.true;
+  });
+
+  it('removed scheduler change of isExpired if validUntil becomes undefined', function () {
+    const model = this.subject();
+    set(model, 'caveats', [createTimeCaveat(Date.now() + 3600)]);
+    this.clock.tick(1 * 1000);
+    set(model, 'caveats', []);
+    this.clock.tick(3601 * 1000);
+
+    expect(get(model, 'isExpired')).to.be.false;
+  });
+
+  it(
+    'has isActive == true if isExpired == false, revoked == false and usageLimitReached == false',
+    function () {
+      const model = this.subject();
+      setProperties(model, {
+        caveats: [createTimeCaveat(Date.now() + 3600)],
+        metadata: {
+          usageCount: 0,
+          usageLimit: 3,
+        },
+        revoked: false,
+      });
+
+      expect(get(model, 'isActive')).to.be.true;
+    }
+  );
+
+  it(
+    'has isActive == true if isExpired == true, revoked == false and usageLimitReached == false',
+    function () {
+      const model = this.subject();
+      setProperties(model, {
+        caveats: [createTimeCaveat(Date.now() - 3600)],
+        metadata: {
+          usageCount: 0,
+          usageLimit: 3,
+        },
+        revoked: false,
+      });
+
+      expect(get(model, 'isActive')).to.be.false;
+    }
+  );
+
+  it(
+    'has isActive == true if isExpired == false, revoked == true and usageLimitReached == false',
+    function () {
+      const model = this.subject();
+      setProperties(model, {
+        caveats: [createTimeCaveat(Date.now() + 3600)],
+        metadata: {
+          usageCount: 0,
+          usageLimit: 3,
+        },
+        revoked: true,
+      });
+
+      expect(get(model, 'isActive')).to.be.false;
+    }
+  );
+
+  it(
+    'has isActive == true if isExpired == false, revoked == false and usageLimitReached == true',
+    function () {
+      const model = this.subject();
+      setProperties(model, {
+        caveats: [createTimeCaveat(Date.now() + 3600)],
+        metadata: {
+          usageCount: 3,
+          usageLimit: 3,
+        },
+        revoked: false,
+      });
+
+      expect(get(model, 'isActive')).to.be.false;
+    }
+  );
+});
+
+function createTimeCaveat(validUntil) {
+  return {
+    type: 'time',
+    validUntil,
+  };
+}
