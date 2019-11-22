5fb5fed8a37683a6ceb15a59cff380d400f7bfe3
Michał Borzęcki <michal.borzecki@hotmail.com>
2019-11-22 13:03:44 +0100

    VFS-5876 merge two-level-sidebar into one-sidebar

--- src/app/components/sidebar-tokens.js
+++ src/app/components/sidebar-tokens.js
@@ -1,5 +1,5 @@
 /**
- * A sidebar for tokens (extension of `two-level-sidebar`)
+ * A sidebar for tokens (extension of `one-sidebar`)
  *
  * @module components/sidebar-tokens
  * @author Michał Borzęcki
@@ -7,11 +7,11 @@
  * @license This software is released under the MIT license cited in 'LICENSE.txt'.
  */
 
-import TwoLevelSidebar from 'onedata-gui-common/components/two-level-sidebar';
-import layout from 'onedata-gui-common/templates/components/two-level-sidebar';
+import OneSidebar from 'onedata-gui-common/components/one-sidebar';
+import layout from 'onedata-gui-common/templates/components/one-sidebar';
 import { computed } from '@ember/object';
 
-export default TwoLevelSidebar.extend({
+export default OneSidebar.extend({
   layout,
   classNames: ['sidebar-tokens'],
 
