5c7f1b1cfb529b74a379e4f0ac66bbfc197efe41
Michał Borzęcki <michal.borzecki@hotmail.com>
2020-02-10 10:56:19 +0100

    VFS-6109 added trimming join tokens

--- src/app/components/content-groups-join-as-subgroup.js
+++ src/app/components/content-groups-join-as-subgroup.js
@@ -9,6 +9,8 @@
 
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
+import trimToken from 'onedata-gui-common/utils/trim-token';
+import computedPipe from 'onedata-gui-common/utils/ember/computed-pipe';
 import I18n from 'onedata-gui-common/mixins/components/i18n';
 
 export default Component.extend(I18n, {
@@ -31,18 +33,25 @@ export default Component.extend(I18n, {
    */
   token: '',
 
+  /**
+   * @type {ComputedProperty<String>}
+   */
+  trimmedToken: computedPipe('token', trimToken),
+
   didInsertElement() {
     this._super(...arguments);
     this.$('#join-group-token').focus();
   },
 
   actions: {
-    joinGroup(token) {
+    joinGroup() {
       const {
         group,
         groupActions,
-      } = this.getProperties('group', 'groupActions');
-      return groupActions.joinGroupAsSubgroup(group, token);
+        trimmedToken,
+      } = this.getProperties('group', 'groupActions', 'trimmedToken');
+
+      return groupActions.joinGroupAsSubgroup(group, trimmedToken);
     },
   },
 });
