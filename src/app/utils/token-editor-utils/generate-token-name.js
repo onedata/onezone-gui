9753570af73a33b89bed8dd8bb385e14a3a3ba3e
Michał Borzęcki <michal.borzecki@hotmail.com>
2020-04-28 10:52:20 +0200

    VFS-6302 added generateTokenName util function

---
+++ src/app/utils/token-editor-utils/generate-token-name.js
@@ -0,0 +1,57 @@
+/**
+ * Generates a default name for token using type, inviteType and invite target name.
+ *
+ * @module utils/token-editor-utils/generate-token-name
+ * @author Michał Borzęcki
+ * @copyright (C) 2020 ACK CYFRONET AGH
+ * @license This software is released under the MIT license cited in 'LICENSE.txt'.
+ */
+
+import moment from 'moment';
+
+const inviteTypeNames = {
+  userJoinGroup: 'inv-usr-grp',
+  groupJoinGroup: 'inv-grp-grp',
+  userJoinSpace: 'inv-usr-spc',
+  groupJoinSpace: 'inv-grp-spc',
+  harvesterJoinSpace: 'inv-hrv-spc',
+  supportSpace: 'support-space',
+  registerOneprovider: 'register-oneprovider',
+  userJoinCluster: 'inv-usr-cls',
+  groupJoinCluster: 'inv-grp-cls',
+  userJoinHarvester: 'inv-usr-hrv',
+  groupJoinHarvester: 'inv-grp-hrv',
+  spaceJoinHarvester: 'inv-spc-hrv',
+};
+
+/**
+ * @param {String} type One of: access, indentity, invite
+ * @param {String} [inviteType] For possible values see keys of `inviteTypeNames` above.
+ *   Needed only when type is 'invite'
+ * @param {String} [inviteTargetName] Invitation target record name. Meaningful only when
+ *   type is 'invite'.
+ * @returns {String}
+ */
+export default function generateTokenName(type, inviteType, inviteTargetName) {
+  const possibleInviteTypes = Object.keys(inviteTypeNames);
+
+  const timeString = moment().format('YYYY.MM.DD-HH.mm');
+  switch (type) {
+    case 'access':
+    case 'identity':
+      return `${type}-${timeString}`;
+    case 'invite': {
+      if (!possibleInviteTypes.includes(inviteType)) {
+        return `${type}-${timeString}`;
+      }
+      let name = inviteTypeNames[inviteType];
+      if (inviteTargetName) {
+        name += `-${inviteTargetName}`;
+      }
+      name += `-${timeString}`;
+      return name;
+    }
+    default:
+      return `token-${timeString}`;
+  }
+}
---
+++ src/tests/unit/utils/token-editor-utils/generate-token-name-test.js
@@ -0,0 +1,110 @@
+import { expect } from 'chai';
+import { describe, it, beforeEach, afterEach } from 'mocha';
+import generateTokenName from 'onezone-gui/utils/token-editor-utils/generate-token-name';
+import sinon from 'sinon';
+import moment from 'moment';
+
+describe('Unit | Utility | token editor utils/generate token name', function () {
+  beforeEach(function () {
+    const now = moment('2020-04-28 10:05:00');
+    this.fakeClock = sinon.useFakeTimers({
+      now: now.valueOf(),
+      shouldAdvanceTime: true,
+    });
+    this.nowString = now.format('YYYY.MM.DD-HH.mm');
+  });
+
+  afterEach(function () {
+    this.fakeClock.restore();
+  });
+
+  it('returns "token-DATE" when no arguments have been passed', function () {
+    const name = generateTokenName();
+    expect(name).to.equal(`token-${this.nowString}`);
+  });
+
+  [
+    'access',
+    'identity',
+    'invite',
+  ].forEach(type => {
+    it(`returns "${type}-DATE" when type is "${type}"`, function () {
+      const name = generateTokenName(type);
+      expect(name).to.equal(`${type}-${this.nowString}`);
+    });
+  });
+
+  it(
+    'returns "invite-DATE" when type is "invite" and inviteType has unknown value',
+    function () {
+      const name = generateTokenName('invite', 'sth');
+      expect(name).to.equal(`invite-${this.nowString}`);
+    }
+  );
+
+  [{
+    inviteType: 'userJoinGroup',
+    nameWithTarget: 'inv-usr-grp-recordName',
+    nameWithoutTarget: 'inv-usr-grp',
+  }, {
+    inviteType: 'groupJoinGroup',
+    nameWithTarget: 'inv-grp-grp-recordName',
+    nameWithoutTarget: 'inv-grp-grp',
+  }, {
+    inviteType: 'userJoinSpace',
+    nameWithTarget: 'inv-usr-spc-recordName',
+    nameWithoutTarget: 'inv-usr-spc',
+  }, {
+    inviteType: 'groupJoinSpace',
+    nameWithTarget: 'inv-grp-spc-recordName',
+    nameWithoutTarget: 'inv-grp-spc',
+  }, {
+    inviteType: 'harvesterJoinSpace',
+    nameWithTarget: 'inv-hrv-spc-recordName',
+    nameWithoutTarget: 'inv-hrv-spc',
+  }, {
+    inviteType: 'supportSpace',
+    nameWithTarget: 'support-space-recordName',
+    nameWithoutTarget: 'support-space',
+  }, {
+    inviteType: 'registerOneprovider',
+    nameWithTarget: 'register-oneprovider-recordName',
+    nameWithoutTarget: 'register-oneprovider',
+  }, {
+    inviteType: 'userJoinCluster',
+    nameWithTarget: 'inv-usr-cls-recordName',
+    nameWithoutTarget: 'inv-usr-cls',
+  }, {
+    inviteType: 'groupJoinCluster',
+    nameWithTarget: 'inv-grp-cls-recordName',
+    nameWithoutTarget: 'inv-grp-cls',
+  }, {
+    inviteType: 'userJoinHarvester',
+    nameWithTarget: 'inv-usr-hrv-recordName',
+    nameWithoutTarget: 'inv-usr-hrv',
+  }, {
+    inviteType: 'groupJoinHarvester',
+    nameWithTarget: 'inv-grp-hrv-recordName',
+    nameWithoutTarget: 'inv-grp-hrv',
+  }, {
+    inviteType: 'spaceJoinHarvester',
+    nameWithTarget: 'inv-spc-hrv-recordName',
+    nameWithoutTarget: 'inv-spc-hrv',
+  }].forEach(({ inviteType, nameWithTarget, nameWithoutTarget }) => {
+    it(
+      `returns "${nameWithoutTarget}-DATE" when type is "invite", inviteType is "${inviteType}" and inviteTargetName is not provided`,
+      function () {
+        const name = generateTokenName('invite', inviteType);
+        expect(name).to.equal(`${nameWithoutTarget}-${this.nowString}`);
+      }
+    );
+
+    it(
+      `returns "${nameWithTarget}-DATE" when type is "invite", inviteType is "${inviteType}" and inviteTargetName is "recordName"`,
+      function () {
+        const name = generateTokenName('invite', inviteType, 'recordName');
+        expect(name).to.equal(`${nameWithTarget}-${this.nowString}`);
+      }
+    );
+  });
+});
