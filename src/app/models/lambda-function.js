249735d2f05fd7372474a3a58185973f58229630
Michał Borzęcki <michal.borzecki@hotmail.com>
2021-04-12 10:23:31 +0200

    VFS-7538 lambda-function model class draft

---
+++ src/app/models/lambda-function.js
@@ -0,0 +1,91 @@
+/**
+ * Lambda function definition (for automation inventory)
+ *
+ * @module models/lambda-function
+ * @author Michał Borzęcki
+ * @copyright (C) 2021 ACK CYFRONET AGH
+ * @license This software is released under the MIT license cited in 'LICENSE.txt'.
+ */
+
+import DS from 'ember-data';
+import attr from 'ember-data/attr';
+import StaticGraphModelMixin from 'onedata-gui-websocket-client/mixins/models/static-graph-model';
+import GraphSingleModelMixin from 'onedata-gui-websocket-client/mixins/models/graph-single-model';
+
+/**
+ * @typedef {Object} OpenfaasOptions
+ * @property {String} dockerImage
+ */
+
+/**
+ * @typedef {Object} OnedataFunctionOptions
+ */
+
+/**
+ * @typedef {Object} MountSpaceOptions
+ * @property {Boolean} enabled
+ * @property {String} mountPath
+ * @property {Boolean} readOnly
+ * @property {String} oneclientOptions
+ */
+
+/**
+ * @typedef {Object} LambdaFunctionArgument
+ * @property {String} name
+ * @property {String} type
+ * @property {Boolean} array
+ * @property {Boolean} optional
+ */
+
+/**
+ * @typedef {Object} LambdaFunctionResult
+ * @property {String} name
+ * @property {String} type
+ * @property {Boolean} array
+ * @property {Boolean} optional
+ */
+
+export default DS.Model.extend(GraphSingleModelMixin, {
+  /**
+   * @type {ComputedProperty<String>}
+   */
+  name: attr('string'),
+
+  /**
+   * @type {ComputedProperty<String>}
+   */
+  shortDescription: attr('string'),
+
+  /**
+   * Is in markdown format
+   * @type {ComputedProperty<String>}
+   */
+  description: attr('string'),
+
+  /**
+   * One of: `openfaas`, `onedataFunction`
+   * @type {ComputedProperty<String>}
+   */
+  engine: attr('string'),
+
+  /**
+   * Its structure depends on the value of `engine` property
+   * @type {ComputedProperty<OpenfaasOptions|OnedataFunctionOptions>}
+   */
+  engineOptions: attr('object'),
+
+  /**
+   * @type {ComputedProperty<MountSpaceOptions>}
+   */
+  mountSpaceOptions: attr('object'),
+
+  /**
+   * @type {ComputedProperty<Array<LambdaFunctionArgument>>}
+   */
+  arguments: attr('array'),
+
+  /**
+   * @type {ComputedProperty<Array<LambdaFunctionResult>>}
+   */
+  results: attr('array'),
+}).reopenClass(StaticGraphModelMixin);
