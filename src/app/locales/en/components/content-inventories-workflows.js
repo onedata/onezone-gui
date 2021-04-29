d47b1bfce94d223096d75d2b151eb49f25f9d9aa
Michał Borzęcki <michal.borzecki@hotmail.com>
2021-04-29 17:53:42 +0200

    VFS-7597 Added creator-view component

--- src/app/locales/en/components/content-inventories-workflows.js
+++ src/app/locales/en/components/content-inventories-workflows.js
@@ -1,9 +1,11 @@
 import listView from './content-inventories-workflows/list-view';
 import workflowsListEntry from './content-inventories-workflows/workflows-list-entry';
 import workflowDetailsForm from './content-inventories-workflows/workflow-details-form';
+import creatorView from './content-inventories-workflows/creator-view';
 
 export default {
   listView,
   workflowsListEntry,
   workflowDetailsForm,
+  creatorView,
 };
---
+++ src/app/locales/en/components/content-inventories-workflows/creator-view.js
@@ -0,0 +1,6 @@
+export default {
+  header: 'Add new workflow',
+  contentInfoTitle: 'Create new workflow',
+  contentInfoText: 'Enter details below to create a new workflow.',
+  submitBtn: 'Create',
+};
