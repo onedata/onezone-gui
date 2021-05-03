8dd5371fc52308759357b13b4b63900e9856c6da
Michał Borzęcki <michal.borzecki@hotmail.com>
2021-05-03 16:20:06 +0200

    VFS-7597 Added loading-view

---
+++ src/app/locales/en/components/content-inventories-workflows/loading-view.js
@@ -0,0 +1,8 @@
+export default {
+  header: {
+    loading: 'Loading workflow...',
+    notFound: 'Workflow not found',
+    forbidden: 'Workflow is not accessible',
+    otherError: 'Workflow cannot be loaded',
+  },
+};
