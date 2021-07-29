b966eefcc322abaf672b49197e4b1ede1b0f6fdc
Michał Borzęcki <michal.borzecki@hotmail.com>
2021-07-29 16:23:28 +0200

    VFS-8076 fix draft

--- src/app/utils/workflow-actions/upload-atm-workflow-schema-action.js
+++ src/app/utils/workflow-actions/upload-atm-workflow-schema-action.js
@@ -13,6 +13,7 @@ import Action from 'onedata-gui-common/utils/action';
 import ActionResult from 'onedata-gui-common/utils/action-result';
 import { inject as service } from '@ember/service';
 import { Promise } from 'rsvp';
+import { defer } from 'rsvp';
 
 export default Action.extend({
   workflowManager: service(),
@@ -38,9 +39,41 @@ export default Action.extend({
   atmInventory: reads('context.atmInventory'),
 
   /**
+   * @type {HTMLInputElement|null}
+   */
+  uploadInputElement: null,
+
+  /**
+   * @type {RSVP.Defer}
+   */
+  uploadFileDefer: null,
+
+  /**
    * @type {Window}
    */
   _window: window,
+
+  /**
+   * @override
+   */
+  init() {
+    this._super(...arguments);
+    this.mountUploadInput();
+  },
+
+  /**
+   * @override
+   */
+  willDestroy() {
+    try {
+      if (this.get('uploadFileDefer')) {
+        this.fileSelectionCancelled();
+      }
+      this.unmountUploadInput();
+    } finally {
+      this._super(...arguments);
+    }
+  },
 
   /**
    * @override
@@ -51,7 +84,11 @@ export default Action.extend({
     let file;
     try {
       file = await this.getJsonFile();
+      console.log('selected');
+      alert('selected');
     } catch (e) {
+      console.log('cancel');
+      alert('cancel');
       result.cancelIfPending();
       return result;
     }
@@ -73,38 +110,109 @@ export default Action.extend({
     ).then(() => result, () => result);
   },
 
-  async getJsonFile() {
-    return await new Promise((resolve, reject) => {
-      const _window = this.get('_window');
+  /**
+   * @private
+   */
+  mountUploadInput() {
+    const {
+      uploadInputElement: existingUploadInputElement,
+      _window,
+    } = this.getProperties('uploadInputElement', '_window');
 
-      // Based on https://stackoverflow.com/a/63773257. It does not detect
-      // "open file" dialog canellation on iOS and it is hard to find any working
-      // solution for that issue. :(
-      let lock = false;
-      const _document = _window.document;
-      const input = _document.createElement('input');
-      input.style.display = 'none';
-      input.setAttribute('type', 'file');
-      input.setAttribute('accept', '.json');
-      input.addEventListener('change', () => {
-        lock = true;
-        const file = input.files[0];
-        resolve(file);
-      }, { once: true });
-      _window.addEventListener('focus', () => {
-        // Wait for input change event to be processed (especially in mobile
-        // browsers and macOS).
-        setTimeout(() => {
-          if (!lock) {
-            reject();
-          }
-        }, 2000);
-      }, { once: true });
+    if (existingUploadInputElement) {
+      return;
+    }
 
-      input.click();
+    const _document = _window.document;
+    const body = _document.body;
+    const input = _document.createElement('input');
+    input.style.display = 'none';
+    input.classList.add('upload-atm-workflow-schema-action-input');
+    input.setAttribute('type', 'file');
+    input.setAttribute('accept', '.json');
+    input.addEventListener('change', () => {
+      if (!input.files || !input.files.length) {
+        return;
+      }
+      const file = input.files[0];
+      this.fileSelected(file);
+      // We need to clear input, to allow user to upload the same file again later
+      // (which, without clearing the input, would not trigger input change).
+      input.value = '';
     });
+    body.appendChild(input);
+    this.set('uploadInputElement', input);
   },
 
+  /**
+   * @private
+   */
+  unmountUploadInput() {
+    const uploadInputElement = this.get('uploadInputElement');
+
+    if (!uploadInputElement) {
+      return;
+    }
+
+    uploadInputElement.parentElement.removeChild(uploadInputElement);
+    this.set('uploadInputElement', null);
+  },
+
+  /**
+   * @private
+   * @returns {Promise<File>}
+   */
+  async getJsonFile() {
+    const {
+      uploadInputElement,
+      uploadFileDefer: existingUploadFileDefer,
+      _window,
+    } = this.getProperties('uploadInputElement', 'uploadFileDefer', '_window');
+    if (existingUploadFileDefer) {
+      this.fileSelectionCancelled();
+    }
+    const newUploadFileDefer = this.set('uploadFileDefer', defer());
+    _window.addEventListener('focus', () => {
+      // Wait for input change event to be processed (especially in mobile
+      // browsers and macOS).
+      setTimeout(() => {
+        if (this.get('uploadFileDefer') === newUploadFileDefer) {
+          this.fileSelectionCancelled();
+        }
+      }, 2000);
+    }, { once: true });
+    uploadInputElement.click();
+    return await newUploadFileDefer.promise;
+  },
+
+  /**
+   * @private
+   * @param {File} file
+   */
+  fileSelected(file) {
+    const uploadFileDefer = this.get('uploadFileDefer');
+    if (uploadFileDefer) {
+      uploadFileDefer.resolve(file);
+      this.set('uploadFileDefer', null);
+    }
+  },
+
+  /**
+   * @private
+   */
+  fileSelectionCancelled() {
+    const uploadFileDefer = this.get('uploadFileDefer');
+    if (uploadFileDefer) {
+      uploadFileDefer.reject();
+      this.set('uploadFileDefer', null);
+    }
+  },
+
+  /**
+   * @private
+   * @param {File} file
+   * @returns {Promise<String>}
+   */
   async getFileContent(file) {
     return await new Promise((resolve, reject) => {
       const reader = new FileReader();
@@ -116,6 +224,11 @@ export default Action.extend({
     });
   },
 
+  /**
+   * @private
+   * @param {String} fileContent
+   * @returns {Object}
+   */
   generateAtmWorkflowPrototype(fileContent) {
     const spec = JSON.parse(fileContent);
 
@@ -126,6 +239,11 @@ export default Action.extend({
     return spec;
   },
 
+  /**
+   * @private
+   * @param {Object} atmWorkflowSchemaPrototype
+   * @returns {Promise<Models.AtmWorkflowSchema>}
+   */
   async createAtmWorkflowSchema(atmWorkflowSchemaPrototype) {
     const {
       workflowManager,
