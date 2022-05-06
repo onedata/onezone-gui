6b5a38d58753f3b402d17551d16a12fb1b8540bd
plgagneska <agawarchol15@gmail.com>
2022-05-06 09:48:27 +0200

    VFS-9163 Add overview to provider tabs, page for config space

--- src/app/components/content-spaces-providers.js
+++ src/app/components/content-spaces-providers.js
@@ -7,7 +7,6 @@
  * @license This software is released under the MIT license cited in 'LICENSE.txt'.
  */
 
-import Component from '@ember/component';
 import { reads } from '@ember/object/computed';
 import { get, computed } from '@ember/object';
 import { inject as service } from '@ember/service';
@@ -15,249 +14,267 @@ import { serializeAspectOptions } from 'onedata-gui-common/services/navigation-s
 import I18n from 'onedata-gui-common/mixins/components/i18n';
 import GlobalActions from 'onedata-gui-common/mixins/components/global-actions';
 import ProvidersColors from 'onedata-gui-common/mixins/components/providers-colors';
-import { collect, conditional } from 'ember-awesome-macros';
+import { collect, conditional, or, raw } from 'ember-awesome-macros';
 import insufficientPrivilegesMessage from 'onedata-gui-common/utils/i18n/insufficient-privileges-message';
+import ContentOneproviderContainerBase from './content-oneprovider-container-base';
 
-export default Component.extend(I18n, GlobalActions, ProvidersColors, {
-  classNames: ['content-spaces-providers'],
+export default ContentOneproviderContainerBase.extend(
+  I18n,
+  GlobalActions,
+  ProvidersColors, {
+    classNames: ['content-spaces-providers'],
 
-  router: service(),
-  i18n: service(),
-  guiUtils: service(),
-  spaceActions: service(),
-  globalClipboard: service(),
-  media: service(),
+    router: service(),
+    i18n: service(),
+    guiUtils: service(),
+    spaceActions: service(),
+    globalClipboard: service(),
+    media: service(),
 
-  i18nPrefix: 'components.contentSpacesProviders',
+    i18nPrefix: 'components.contentSpacesProviders',
 
-  /**
-   * @virtual
-   * @type {Space}
-   */
-  space: undefined,
+    /**
+     * @virtual
+     * @type {Space}
+     */
+    space: undefined,
 
-  /**
-   * Oneprovider model passed to cease support modal
-   * @type {Models.Provider}
-   */
-  ceaseModalProvider: null,
+    /**
+     * Oneprovider model passed to cease support modal
+     * @type {Models.Provider}
+     */
+    ceaseModalProvider: null,
 
-  /**
-   * @type {boolean}
-   */
-  ceaseModalOpened: false,
+    /**
+     * @type {boolean}
+     */
+    ceaseModalOpened: false,
 
-  /**
-   * @override 
-   * @type {Ember.ComputedProperty<string>}
-   */
-  globalActionsTitle: computed(function () {
-    return this.t('space');
-  }),
-
-  // TODO: undefined possible?
-  providersProxy: reads('space.providerList.list'),
-
-  // TODO: it will recompute everything if color changes
-  providersData: computed(
-    'providersProxy.{content.[],isFulfilled}',
-    'providersColors',
-    function getProvidersData() {
-      const providersProxy = this.get('providersProxy');
-      if (get(providersProxy, 'isFulfilled')) {
-        const providerColors = this.get('providersColors');
-        return providersProxy.map(provider => ({
-          provider,
-          color: providerColors[get(provider, 'entityId')],
-        }));
-      }
+    /**
+     * @override 
+     * @type {Ember.ComputedProperty<string>}
+     */
+    globalActionsTitle: computed(function () {
+      return this.t('space');
     }),
 
-  /**
-   * @type {Ember.ComputedProperty <boolean>}
-   */
-  hasAddSupportPrivilege: reads('space.privileges.addSupport'),
+    // TODO: undefined possible?
+    providersProxy: reads('space.providerList.list'),
 
-  /**
-   * @type {Ember.ComputedProperty <boolean>}
-   */
-  hasRemoveSupportPrivilege: reads('space.privileges.removeSupport'),
+    oneproviderId: or('navigationState.aspectOptions.oneproviderId', raw('overview')),
 
-  /**
-   * @type {Ember.ComputedProperty<AspectAction>}
-   */
-  openAddStorageAction: computed('hasAddSupportPrivilege', function () {
-    const {
-      hasAddSupportPrivilege,
-      i18n,
-    } = this.getProperties('hasAddSupportPrivilege', 'i18n');
-    return {
-      action: () => this.send('openAddStorage'),
-      title: this.t('addStorage'),
-      class: 'open-add-storage btn-add-support',
-      buttonStyle: 'default',
-      icon: 'provider-add',
-      disabled: !hasAddSupportPrivilege,
-      tip: !hasAddSupportPrivilege ? insufficientPrivilegesMessage({
-        i18n,
-        modelName: 'space',
-        privilegeFlag: 'space_add_support',
-      }) : null,
-    };
-  }),
+    // TODO: it will recompute everything if color changes
+    providersData: computed(
+      'providersProxy.{content.[],isFulfilled}',
+      'providersColors',
+      function getProvidersData() {
+        const providersProxy = this.get('providersProxy');
+        if (get(providersProxy, 'isFulfilled')) {
+          const providerColors = this.get('providersColors');
+          return providersProxy.map(provider => ({
+            provider,
+            color: providerColors[get(provider, 'entityId')],
+          }));
+        }
+      }
+    ),
 
-  globalActions: computed('openAddStorageAction', function getGlobalActions() {
-    return [this.get('openAddStorageAction')];
-  }),
+    /**
+     * @type {Ember.ComputedProperty <boolean>}
+     */
+    hasAddSupportPrivilege: reads('space.privileges.addSupport'),
 
-  ceaseOneproviderSupportAction: computed(
-    'space.privileges.removeSupport',
-    function ceaseOneproviderSupportAction() {
+    /**
+     * @type {Ember.ComputedProperty <boolean>}
+     */
+    hasRemoveSupportPrivilege: reads('space.privileges.removeSupport'),
+
+    /**
+     * @type {Ember.ComputedProperty<AspectAction>}
+     */
+    openAddStorageAction: computed('hasAddSupportPrivilege', function () {
       const {
-        hasRemoveSupportPrivilege,
+        hasAddSupportPrivilege,
         i18n,
-      } = this.getProperties('hasRemoveSupportPrivilege', 'i18n');
+      } = this.getProperties('hasAddSupportPrivilege', 'i18n');
       return {
-        icon: 'leave-space',
-        text: this.t('ceaseSupportItem'),
-        class: 'cease-oneprovider-support-btn',
-        action: (provider) => this.openCeaseModal(provider),
-        isDisabled: !hasRemoveSupportPrivilege,
-        tip: !hasRemoveSupportPrivilege ? insufficientPrivilegesMessage({
+        action: () => this.send('openAddStorage'),
+        title: this.t('addStorage'),
+        class: 'open-add-storage btn-add-support',
+        buttonStyle: 'default',
+        icon: 'provider-add',
+        disabled: !hasAddSupportPrivilege,
+        tip: !hasAddSupportPrivilege ? insufficientPrivilegesMessage({
           i18n,
           modelName: 'space',
-          privilegeFlag: 'space_remove_support',
+          privilegeFlag: 'space_add_support',
         }) : null,
       };
-    }
-  ),
+    }),
 
-  /**
-   * @type {Ember.ComputedProperty<Action>}
-   */
-  copyProviderIdAction: computed(function copyProviderIdAction() {
-    return {
-      icon: 'copy',
-      text: this.t('copyProviderIdAction'),
-      class: 'copy-provider-id-action-trigger',
-      action: (provider) => this.get('globalClipboard').copy(
-        get(provider, 'entityId'),
-        this.t('providerId')
-      ),
-    };
-  }),
+    globalActions: computed('openAddStorageAction', function getGlobalActions() {
+      return [this.get('openAddStorageAction')];
+    }),
 
-  /**
-   * @type {Ember.ComputedProperty<Action>}
-   */
-  copyProviderDomainAction: computed(function copyProviderDomainAction() {
-    return {
-      icon: 'copy',
-      text: this.t('copyProviderDomainAction'),
-      class: 'copy-provider-domain-action-trigger',
-      action: (provider) => this.get('globalClipboard').copy(
-        get(provider, 'domain'),
-        this.t('providerDomain')
-      ),
-    };
-  }),
-
-  /**
-   * @type {Ember.ComputedProperty<Action>}
-   */
-  browseFilesAction: computed(function browseFilesAction() {
-    return {
-      icon: 'browser-directory',
-      text: this.t('browseFilesAction'),
-      class: 'visit-provider-action-trigger',
-      action: (provider) => this.browseFiles(provider),
-    };
-  }),
-
-  /**
-   * @type {Ember.ComputedProperty<Action>}
-   */
-  showDetailsAction: computed(function showDetailsAction() {
-    return {
-      icon: 'provider',
-      text: this.t('showDetailsAction'),
-      class: 'show-provider-details-action-trigger',
-      action: (provider) => this.showDetails(provider),
-    };
-  }),
-
-  /**
-   * @type {ComputedProperty<Array<Action>>}
-   */
-  providerActions: conditional(
-    'media.isMobile',
-    collect(
-    'browseFilesAction',
-    'showDetailsAction',
-    'copyProviderIdAction',
-    'copyProviderDomainAction',
-    'ceaseOneproviderSupportAction'
-    ), collect(
-    'browseFilesAction',
-    'showDetailsAction',
-    'ceaseOneproviderSupportAction'
-    )
-  ),
-
-  browseFiles(provider) {
-    const {
-      guiUtils,
-      router,
-      space,
-    } = this.getProperties('guiUtils', 'router', 'space');
-    return router.transitionTo(
-      'onedata.sidebar.content.aspect',
-      'spaces',
-      guiUtils.getRoutableIdFor(space),
-      'data', {
-        queryParams: {
-          options: serializeAspectOptions({
-            oneproviderId: get(provider, 'entityId'),
-          }),
-        },
+    ceaseOneproviderSupportAction: computed(
+      'space.privileges.removeSupport',
+      function ceaseOneproviderSupportAction() {
+        const {
+          hasRemoveSupportPrivilege,
+          i18n,
+        } = this.getProperties('hasRemoveSupportPrivilege', 'i18n');
+        return {
+          icon: 'leave-space',
+          text: this.t('ceaseSupportItem'),
+          class: 'cease-oneprovider-support-btn',
+          action: (provider) => this.openCeaseModal(provider),
+          isDisabled: !hasRemoveSupportPrivilege,
+          tip: !hasRemoveSupportPrivilege ? insufficientPrivilegesMessage({
+            i18n,
+            modelName: 'space',
+            privilegeFlag: 'space_remove_support',
+          }) : null,
+        };
       }
-    );
-  },
+    ),
 
-  showDetails(provider) {
-    const {
-      guiUtils,
-      router,
-    } = this.getProperties('guiUtils', 'router');
-    return router.transitionTo(
-      'onedata.sidebar.content',
-      'providers',
-      guiUtils.getRoutableIdFor(provider),
-    );
-  },
+    /**
+     * @type {Ember.ComputedProperty<Action>}
+     */
+    copyProviderIdAction: computed(function copyProviderIdAction() {
+      return {
+        icon: 'copy',
+        text: this.t('copyProviderIdAction'),
+        class: 'copy-provider-id-action-trigger',
+        action: (provider) => this.get('globalClipboard').copy(
+          get(provider, 'entityId'),
+          this.t('providerId')
+        ),
+      };
+    }),
 
-  openCeaseModal(provider) {
-    this.setProperties({
-      ceaseModalOpened: true,
-      ceaseModalProvider: provider,
-    });
-  },
+    /**
+     * @type {Ember.ComputedProperty<Action>}
+     */
+    copyProviderDomainAction: computed(function copyProviderDomainAction() {
+      return {
+        icon: 'copy',
+        text: this.t('copyProviderDomainAction'),
+        class: 'copy-provider-domain-action-trigger',
+        action: (provider) => this.get('globalClipboard').copy(
+          get(provider, 'domain'),
+          this.t('providerDomain')
+        ),
+      };
+    }),
 
-  actions: {
-    openAddStorage() {
-      return this.get('router').transitionTo(
+    /**
+     * @type {Ember.ComputedProperty<Action>}
+     */
+    browseFilesAction: computed(function browseFilesAction() {
+      return {
+        icon: 'browser-directory',
+        text: this.t('browseFilesAction'),
+        class: 'visit-provider-action-trigger',
+        action: (provider) => this.browseFiles(provider),
+      };
+    }),
+
+    /**
+     * @type {Ember.ComputedProperty<Action>}
+     */
+    showDetailsAction: computed(function showDetailsAction() {
+      return {
+        icon: 'provider',
+        text: this.t('showDetailsAction'),
+        class: 'show-provider-details-action-trigger',
+        action: (provider) => this.showDetails(provider),
+      };
+    }),
+
+    /**
+     * @type {ComputedProperty<Array<Action>>}
+     */
+    providerActions: conditional(
+      'media.isMobile',
+      collect(
+        'browseFilesAction',
+        'showDetailsAction',
+        'copyProviderIdAction',
+        'copyProviderDomainAction',
+        'ceaseOneproviderSupportAction'
+      ), collect(
+        'browseFilesAction',
+        'showDetailsAction',
+        'ceaseOneproviderSupportAction'
+      )
+    ),
+
+    browseFiles(provider) {
+      const {
+        guiUtils,
+        router,
+        space,
+      } = this.getProperties('guiUtils', 'router', 'space');
+      return router.transitionTo(
         'onedata.sidebar.content.aspect',
-        'support'
+        'spaces',
+        guiUtils.getRoutableIdFor(space),
+        'data', {
+          queryParams: {
+            options: serializeAspectOptions({
+              oneproviderId: get(provider, 'entityId'),
+            }),
+          },
+        }
       );
     },
-    openRemoveSpace() {
-      const {
-        space,
-        spaceActions,
-      } = this.getProperties('space', 'spaceActions');
 
-      this.set('ceaseModalOpened', false);
-      spaceActions.createRemoveSpaceAction({ space }).execute();
+    showDetails(provider) {
+      const {
+        guiUtils,
+        router,
+      } = this.getProperties('guiUtils', 'router');
+      return router.transitionTo(
+        'onedata.sidebar.content',
+        'providers',
+        guiUtils.getRoutableIdFor(provider),
+      );
     },
-  },
-});
+
+    openCeaseModal(provider) {
+      this.setProperties({
+        ceaseModalOpened: true,
+        ceaseModalProvider: provider,
+      });
+    },
+
+    actions: {
+      openAddStorage() {
+        return this.get('router').transitionTo(
+          'onedata.sidebar.content.aspect',
+          'support'
+        );
+      },
+      openRemoveSpace() {
+        const {
+          space,
+          spaceActions,
+        } = this.getProperties('space', 'spaceActions');
+
+        this.set('ceaseModalOpened', false);
+        spaceActions.createRemoveSpaceAction({ space }).execute();
+      },
+
+      oneproviderIdChanged(oneproviderId, replaceHistory) {
+        if (oneproviderId === 'overview') {
+          this.get('navigationState').setRouteAspectOptions(null, replaceHistory);
+        } else {
+          this.get('navigationState').changeRouteAspectOptions({
+            oneproviderId,
+          }, replaceHistory);
+        }
+
+      },
+    },
+  });
---
+++ src/app/components/overview-oneprovider-view-container.js
@@ -0,0 +1,358 @@
+/**
+ * For given space show basic information and let user select the Oneprovider 
+ * and give a place where some component (typically remote component from Oneprovider) 
+ * will be rendered.
+ * 
+ * @module components/overview-oneprovider-view-container
+ * @author Agnieszka Warchoł
+ * @copyright (C) 2022 ACK CYFRONET AGH
+ * @license This software is released under the MIT license cited in 'LICENSE.txt'.
+ */
+
+import Component from '@ember/component';
+import EmberObject, { get, computed, observer } from '@ember/object';
+import { reads, not } from '@ember/object/computed';
+import { promise, notEmpty, array, raw, tag, conditional, gt } from 'ember-awesome-macros';
+import safeExec from 'onedata-gui-common/utils/safe-method-execution';
+import { next } from '@ember/runloop';
+import { inject as service } from '@ember/service';
+import createPropertyComparator from 'onedata-gui-common/utils/create-property-comparator';
+import I18n from 'onedata-gui-common/mixins/components/i18n';
+import notImplementedIgnore from 'onedata-gui-common/utils/not-implemented-ignore';
+import { guidFor } from '@ember/object/internals';
+import isStandaloneGuiOneprovider from 'onedata-gui-common/utils/is-standalone-gui-oneprovider';
+import ChooseDefaultOneprovider from 'onezone-gui/mixins/choose-default-oneprovider';
+import { resolve } from 'rsvp';
+
+const nameComparator = createPropertyComparator('name');
+
+function sortedOneprovidersList(list) {
+  return [...list].sort(nameComparator);
+}
+
+const OneproviderTabItem = EmberObject.extend({
+  /**
+   * @virtual
+   * @type {models.Provider}
+   */
+  provider: undefined,
+  type: 'provider',
+  icon: 'provider',
+  id: reads('provider.entityId'),
+  name: reads('provider.name'),
+  version: reads('provider.version'),
+  domain: reads('provider.domain'),
+  entityId: reads('provider.entityId'),
+  disabled: not('provider.online'),
+  elementClass: computed('provider.online', function elementClass() {
+    return `provider-${this.get('provider.online') ? 'on' : 'off'}line`;
+  }),
+});
+
+const OverviewTabItem = EmberObject.extend({
+  id: 'overview',
+  entityId: 'overview',
+  name: 'Overview',
+  icon: 'overview',
+  class: 'overview',
+  type: 'overview',
+});
+
+export default Component.extend(I18n, ChooseDefaultOneprovider, {
+  tagName: '',
+
+  /**
+   * @override
+   */
+  i18nPrefix: 'components.oneproviderViewContainer',
+
+  pointerEvents: service(),
+
+  /**
+   * View to show in legacy Oneprovider, eg.spaces, transfers, shares
+   * @virtual optional
+   * @deprecated TODO: VFS-7573 remove support for v19 Oneproviders
+   * @type {String}
+   */
+  resourceType: '',
+
+  /**
+   * Space selected in sidebar to show its embedded content using one
+   * of available Oneproviders.
+   * @virtual
+   * @type {models.Space}
+   */
+  space: undefined,
+
+  /**
+   * @virtual
+   * @type {Function}
+   */
+  oneproviderIdChanged: notImplementedIgnore,
+
+  /**
+   * EntityId of one of the Oneproviders that support `space` which will be used
+   * to render embedded component
+   * @virtual
+   * @type {String}
+   */
+  oneproviderId: undefined,
+
+  /**
+   * @virtual optional
+   * @type {String}
+   */
+  tabBarClass: '',
+
+  /**
+   * In collapsed mode, the currently chosen Oneprovider is displayed and
+   * an option to show full options selector
+   * @type {boolean}
+   */
+  isTabBarCollapsed: true,
+
+  /**
+   * @type {boolean}
+   */
+  mapSelectorEnabled: true,
+
+  /**
+   * @type {boolean}
+   */
+  isMapExpanded: false,
+
+  /**
+   * NOTICE: it does not observe providers array, because we do not want to reload view
+   * (proxy goes to pending) when the list changes.
+   * @type {ComputedProperty<Models.Provider>}
+   */
+  selectedProviderProxy: promise.object(computed(
+    'validatedOneproviderIdProxy',
+    function selectedProviderProxy() {
+      const {
+        providers,
+        validatedOneproviderIdProxy,
+      } = this.getProperties('providers', 'validatedOneproviderIdProxy');
+      return validatedOneproviderIdProxy.then(validatedOneproviderId => {
+        if (validatedOneproviderId === 'overview') {
+          return resolve(this.get('overviewProviderItems')[0]);
+        }
+        return providers.findBy('entityId', validatedOneproviderId);
+      });
+    }
+  )),
+
+  selectedProvider: reads('selectedProviderProxy.content'),
+
+  oneproviderViewProxy: promise.object(promise.all(
+    'isEmbeddableOneproviderProxy',
+    'selectedProviderProxy'
+  )),
+
+  /**
+   * `baseUrl` property for embedded component container.
+   * It is URL with path to selected Oneprovider served web application.
+   * @type {ComputedProperty<string>}
+   */
+  contentIframeBaseUrl: reads('selectedProvider.onezoneHostedBaseUrl'),
+
+  /**
+   * True if there are more than one Oneprovider to select
+   * @type {ComputedProperty<boolean>}
+   */
+  multiOneproviders: gt('providers.length', raw(1)),
+
+  /**
+   * State of Oneproviders selector - are there more than one to select?
+   * One of: single, multi.
+   * @type {ComputedProperty<String>}
+   */
+  collapsedSelectorState: conditional(
+    'multiOneproviders',
+    raw('multi'),
+    raw('single'),
+  ),
+
+  /**
+   * @type {ComputedProperty<String>}
+   */
+  componentGuid: computed(function componentGuid() {
+    return guidFor(this);
+  }),
+
+  /**
+   * @type {ComputedProperty<String>}
+   */
+  collapsedSelectorHintTriggerClass: tag `collapsed-selector-hint-trigger-${'componentGuid'}`,
+
+  /**
+   * @type {ComputedProperty<String>}
+   */
+  hintTriggersConfiguration: tag `.${'collapsedSelectorHintTriggerClass'}`,
+
+  validatedOneproviderIdProxy: promise.object(computed(
+    'oneproviderId',
+    function validatedOneproviderIdProxy() {
+      const oneproviderId = this.get('oneproviderId');
+      if (oneproviderId) {
+        return resolve(oneproviderId);
+      } else {
+        return this.chooseDefaultOneprovider().then(defaultOneprovider => {
+          if (defaultOneprovider) {
+            return resolve(get(defaultOneprovider, 'entityId'));
+          } else {
+            return resolve(null);
+          }
+        });
+      }
+    }
+  )),
+
+  providers: computed('space.providerList.list.@each.name', function providers() {
+    return sortedOneprovidersList(this.get('space.providerList.list').toArray());
+  }),
+
+  // TODO: handle deletion of currently selected provider
+  selectedProviderItem: computed(
+    'selectedProvider',
+    'providerItems',
+    function selectedProviderItem() {
+      const {
+        selectedProvider,
+        overviewProviderItems,
+      } = this.getProperties('selectedProvider', 'overviewProviderItems');
+      if (selectedProvider) {
+        const providerEntityId = get(selectedProvider, 'entityId');
+        return overviewProviderItems.findBy('id', providerEntityId);
+      }
+    }
+  ),
+
+  providerItems: array.map(
+    'providers',
+    provider => OneproviderTabItem.create({ provider })
+  ),
+
+  overviewProviderItems: array.concat(
+    [OverviewTabItem.create()],
+    'providerItems',
+  ),
+
+  /**
+   * @type {ComputedProperty<boolean>}
+   */
+  showAllOfflineInfo: computed(
+    'selectedProvider',
+    'providers.@each.online',
+    function showAllOfflineInfo() {
+      return !this.get('selectedProvider') && !this.get('providers').isAny('online');
+    }
+  ),
+
+  hasSupport: notEmpty('providers'),
+
+  /**
+   * @type {ComputedProperty<PromiseObject<Array<Model.Provider>>>}
+   */
+  initialProvidersListProxy: promise.object(
+    computed('space.providerList', function initialProvidersListProxy() {
+      return this.get('space').getRelation('providerList')
+        .then(providerList => get(providerList, 'list'))
+        .then(list => sortedOneprovidersList(list.toArray()));
+    })
+  ),
+
+  isEmbeddableOneproviderProxy: promise.object(
+    computed(
+      'initialProvidersListProxy',
+      'selectedProvider.versionProxy',
+      function isEmbeddableOneproviderProxy() {
+        return this.get('initialProvidersListProxy').then(() => {
+          const selectedProvider = this.get('selectedProvider');
+          if (selectedProvider) {
+            if (selectedProvider.id === 'overview') {
+              return selectedProvider;
+            }
+            return get(selectedProvider, 'versionProxy').then(version => {
+              return !isStandaloneGuiOneprovider(version);
+            });
+          } else {
+            return null;
+          }
+        });
+      }),
+  ),
+
+  isEmbeddableOneprovider: reads('isEmbeddableOneproviderProxy.content'),
+
+  /**
+   * When there is no Oneprovider selected (or there is no Oneprovider at all)
+   * we should observe the list to set the first online Oneprovider when it is
+   * async added.
+   */
+  observeOnlineProvider: observer('providers.[]', function observeOnlineProvider() {
+    const {
+      selectedProvider,
+      providers,
+    } = this.getProperties('selectedProvider', 'providers');
+    if (!providers.includes(selectedProvider)) {
+      return this.selectDefaultProvider();
+    }
+  }),
+
+  init() {
+    this._super(...arguments);
+    this.get('initialProvidersListProxy').then(list => {
+      const oneproviderId = this.get('oneproviderId');
+      if (!oneproviderId) {
+        return this.selectDefaultProvider(list);
+      }
+    });
+    next(() => {
+      safeExec(this, 'set', 'pointerEvents.pointerNoneToMainContent', true);
+    });
+  },
+
+  willDestroyElement() {
+    this._super(...arguments);
+    next(() => {
+      safeExec(this, 'set', 'pointerEvents.pointerNoneToMainContent', false);
+    });
+  },
+
+  selectDefaultProvider(providers = this.get('providers')) {
+    return this.chooseDefaultOneprovider(providers).then(defaultProvider => {
+      if (defaultProvider) {
+        const providerId = get(defaultProvider, 'entityId');
+        this.get('oneproviderIdChanged')(
+          providerId,
+          true,
+        );
+      }
+    });
+  },
+
+  actions: {
+    selectedProviderChanged(providerItem) {
+      this.send('onToggleExpandMap', false);
+      const entityId =
+        get(providerItem, 'entityId') || get(providerItem, 'id');
+      if (entityId === 'overview') {
+        this.get('oneproviderIdChanged')(entityId);
+        return true;
+      }
+      const provider = this.get('providers').findBy('entityId', entityId);
+      if (provider) {
+        this.setBrowserDefaultOneproviderId(entityId);
+        this.get('oneproviderIdChanged')(entityId);
+      } else {
+        this.set('selectedItem', entityId);
+        // TODO: show error if cannot find the selected provider on list
+        return false;
+      }
+    },
+    onToggleExpandMap(expand) {
+      this.set('isMapExpanded', expand);
+    },
+  },
+});
