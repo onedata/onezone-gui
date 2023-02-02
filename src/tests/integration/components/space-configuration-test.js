7522284ab06a10b6834050f4caf8c62a060a4e66
Jakub Liput <jakub.liput@gmail.com>
2023-02-02 13:45:10 +0100

    VFS-10481 Fixed wrong imports in tests

--- src/tests/integration/components/space-configuration-test.js
+++ src/tests/integration/components/space-configuration-test.js
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 import { describe, it } from 'mocha';
 import { setupRenderingTest } from 'ember-mocha';
-import hbs from 'htmlbars-inline-precompile';
+import { hbs } from 'ember-cli-htmlbars';
 import { assert } from '@ember/debug';
 import { render, find, click, fillIn } from '@ember/test-helpers';
 import createSpace from '../../helpers/create-space';
