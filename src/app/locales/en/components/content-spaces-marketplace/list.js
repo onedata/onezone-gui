0befb181a949d89dff17b1bb1b1b46e7e2c4942c
Jakub Liput <jakub.liput@gmail.com>
2023-02-14 10:42:44 +0100

    VFS-10407 Improved search and tags filter UX (placeholders, styles)

--- src/app/locales/en/components/content-spaces-marketplace/list.js
+++ src/app/locales/en/components/content-spaces-marketplace/list.js
@@ -1,3 +1,5 @@
 export default {
   noMatchingResults: 'There are no spaces in marketplace that match your search.',
+  searchPlaceholder: 'Search by space name, organization or description...',
+  tagsFilterPlaceholder: 'Filter spaces by tags...',
 };
