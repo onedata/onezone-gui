203ebc66e93f71bc71810213fd4285a10c37021f
Michał Borzęcki <m.borzecki@cyfronet.pl>
2023-03-16 10:41:43 +0100

    VFS-7873 Fixed translation

--- src/app/locales/en/components/modals/apply-atm-record-dump-modal/operation-form.js
+++ src/app/locales/en/components/modals/apply-atm-record-dump-modal/operation-form.js
@@ -20,11 +20,11 @@ export default {
       },
       revisionConflictWarning: {
         upload: {
-          atmLambda: 'Selected lambda already has revision #{{revisionNumber}}. As lambda revisions cannot be overwritten, the revision from the uploaded file will be assigned the next free number ({{nextFreeRevisionNumber}}).',
+          atmLambda: 'Selected lambda already has revision #{{revisionNumber}}. As lambda revisions cannot be overwritten, the revision from the uploaded file will be assigned the next free number (#{{nextFreeRevisionNumber}}).',
           atmWorkflowSchema: 'Selected workflow already has revision #{{revisionNumber}}. It will be irreversibly replaced by the revision from the uploaded file.',
         },
         duplication: {
-          atmLambda: 'Selected lambda already has revision #{{revisionNumber}}. As lambda revisions cannot be overwritten, the revision from the source lambda will be assigned the next free number ({{nextFreeRevisionNumber}}).',
+          atmLambda: 'Selected lambda already has revision #{{revisionNumber}}. As lambda revisions cannot be overwritten, the revision from the source lambda will be assigned the next free number (#{{nextFreeRevisionNumber}}).',
           atmWorkflowSchema: 'Selected workflow already has revision #{{revisionNumber}}. It will be irreversibly replaced by the revision from the source workflow.',
         },
       },
