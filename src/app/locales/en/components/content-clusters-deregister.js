0062075947f1f6544e14c9168a77fad05a4fa433
plgagneska <agawarchol15@gmail.com>
2023-09-21 14:11:03 +0200

    VFS-11129 Change storage to storage backend in translations

--- src/app/locales/en/components/content-clusters-deregister.js
+++ src/app/locales/en/components/content-clusters-deregister.js
@@ -1,6 +1,6 @@
 export default {
   header: 'Deregister Oneprovider',
-  text: 'Deregistering this Oneprovider from Onezone will cease its support for all spaces and the data kept on its storages will be irreversibly lost (except for those with storage synchronization enabled). As a result, certain users and groups may permanently lose their data.',
+  text: 'Deregistering this Oneprovider from Onezone will cease its support for all spaces and the data kept on its storage backends will be irreversibly lost (except for those with storage synchronization enabled). As a result, certain users and groups may permanently lose their data.',
   cannotUndone: 'This operation cannot be undone.',
   statsPrefix: 'Current statistics for Oneprovider',
   statsSupportedSpaces: 'Supported spaces',
