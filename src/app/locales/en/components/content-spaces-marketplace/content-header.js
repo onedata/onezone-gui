1635b913666e8d92af302486ea48599c2065bcb4
Jakub Liput <jakub.liput@gmail.com>
2023-12-18 19:37:21 +0100

    VFS-11229 Added some Space Marketplace tooltips

--- src/app/locales/en/components/content-spaces-marketplace/content-header.js
+++ src/app/locales/en/components/content-spaces-marketplace/content-header.js
@@ -1,5 +1,11 @@
+import { marketplaceGeneralIntro } from '../-space-marketplace';
+
+const marketplaceViewDescription =
+  'This view presents a browser of all spaces available in the Space Marketplace. You may request access to any advertised space, but the acceptance of your request depends on the decision of the space maintainer. Upon approval, you will become a member of the space, with privileges regulated by the maintainer.';
+
 export default {
   header: 'Space Marketplace',
+  headerTip: `<p>${marketplaceGeneralIntro}</p><p>${marketplaceViewDescription}</p>`,
   refreshAdvertisedSpacesListAction: {
     title: 'Refresh',
   },
