a0991ff47ef045d2d13077fc87a47a6e0440aa8d
Jakub Liput <jakub.liput@gmail.com>
2023-12-20 13:52:01 +0100

    VFS-11229 PR fixes

--- src/app/locales/en/components/-space-marketplace.js
+++ src/app/locales/en/components/-space-marketplace.js
@@ -4,4 +4,4 @@ export const marketplaceGeneralIntro =
   'Space Marketplace allows connecting data producers and data consumers by enabling Onedata space advertising and discovery.';
 
 export const marketplaceGeneralDescription =
-  `<p>${marketplaceGeneralIntro}</p><p>Spaces can be advertised so that any user authenticated in the Onezone can discover them in the Marketplace and request access. Requests are being sent to the maintainer's contact e-mail address using an automated mailing system. The maintainer decides whether to grant or reject access requests.</p><p>At any time, the space can be withdrawn from the Marketplace.</p>`;
+  `<p>${marketplaceGeneralIntro}</p><p>Spaces can be advertised so that any user authenticated in the Onezone can discover them in the Marketplace and request access. The space maintainer is notified (by email) of every new request and then decides whether to grant or reject access.</p><p>At any time, the space can be withdrawn from the Marketplace.</p>`;
