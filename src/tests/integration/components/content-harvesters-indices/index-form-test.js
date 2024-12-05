6265650949d3d6c034254fde7aee9f32dfaf0591
plgagneska <agawarchol15@gmail.com>
2024-12-05 14:11:07 +0100

    VFS-12528 Fixed test

--- src/tests/integration/components/content-harvesters-indices/index-form-test.js
+++ src/tests/integration/components/content-harvesters-indices/index-form-test.js
@@ -85,7 +85,7 @@ describe('Integration | Component | content-harvesters-indices/index-form', func
           'Specifies what types of file metadata should be harvested in this index. At least one type must be given.'
         );
         expect(await basicTooltip.getText()).to.equal(
-          'Key-value pairs representing extended file attributes (xattrs).'
+          'Extended attributes — textual or binary key-value pairs for storing additional metadata, compatible with POSIX extended attributes.'
         );
       }
     );
