96b19ad3030c2c46df85d4f476faf64207642837
Jakub Liput <jakub.liput@gmail.com>
2025-05-08 21:28:14 +0200

    VFS-12765 Added providers-map-tile from common

--- src/app/components/providers-map-tile.js
+++ src/app/components/providers-map-tile.js
@@ -8,12 +8,10 @@
 
 import Component from '@ember/component';
 import { computed } from '@ember/object';
-import layout from '../templates/components/providers-map-tile';
 import I18n from 'onedata-gui-common/mixins/i18n';
 import ProvidersColors from 'onedata-gui-common/mixins/components/providers-colors';
 
 export default Component.extend(I18n, ProvidersColors, {
-  layout,
   tagName: '',
 
   /**
---
+++ src/app/locales/en/components/providers-map-tile.js
@@ -0,0 +1,4 @@
+export default {
+  title: 'Oneproviders',
+  moreText: 'More',
+};
