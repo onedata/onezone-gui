53a1fb36bd0688b73290a55e1714d6a35b7391b1
Jakub Liput <jakub.liput@gmail.com>
2025-05-23 10:17:33 +0200

    VFS-12759 ESLint fixes

--- src/app/services/cluster-manager.js
+++ src/app/services/cluster-manager.js
@@ -2,18 +2,14 @@
  * Provides data and operations for routes and components associated with clusters tab.
  *
  * @author Jakub Liput
- * @copyright (C) 2018 ACK CYFRONET AGH
+ * @copyright (C) 2018-2025 ACK CYFRONET AGH
  * @license This software is released under the MIT license cited in 'LICENSE.txt'.
  */
 
 import Service, { inject as service } from '@ember/service';
 import { get } from '@ember/object';
 import gri from 'onedata-gui-websocket-client/utils/gri';
-import {
-  Promise,
-  resolve,
-  all as allFulfilled,
-} from 'rsvp';
+import { Promise, resolve } from 'rsvp';
 import ignoreForbiddenError from 'onedata-gui-common/utils/ignore-forbidden-error';
 import { entityType as clusterEntityType } from 'onezone-gui/models/cluster';
 
