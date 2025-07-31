36ac4e52e58e81150d3b95fd3612f1f8db91e0a4
Jakub Liput <jakub.liput@gmail.com>
2025-07-31 13:04:12 +0200

    VFS-12934 PR fixes

--- src/app/services/group-manager.js
+++ src/app/services/group-manager.js
@@ -96,7 +96,7 @@ export default Service.extend({
       },
     };
     const group = await this.store.createRecord('group', createData).save();
-    this.reloadList();
+    await this.reloadList();
     return group;
   },
 
